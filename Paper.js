ffab2e958f75bc923c24774f83558add5d5b7ee0
TanviKokate <75251802+TanviKokate@users.noreply.github.com>
2021-01-20 21:30:19 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,27 @@
+class Paper { 
+    constructor(x, y, r) {
+        var options = {
+            isStatic : false,
+            restitution : 0.3,
+            friction : 0.5,
+            density : 1.2
+        }
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.body = Bodies.circle(this.x, this.y, this.r, options);
+        this.image = loadImage("Paper.png");
+        World.add(world, this.body)
+    }
+     display(){
+         var paperpos = this.body.position;
+         var angle = this.body.angle;
+
+         push();
+         translate(paperpos.x, paperpos.y + 42);
+         rotate(angle);
+         imageMode(CENTER);
+         image(this.image, 0, 0, this.r, this.r);
+         pop();
+     }
+}
---
+++ sketch.js
@@ -0,0 +1,52 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ground;
+var paper;
+var dustbin, dustbin_Image;
+var leftBody, rightBody, bottomBody;
+
+function preload(){
+  dustbin_Image = loadImage("dustbingreen.png");
+}
+
+function setup() {
+	createCanvas(1000, 500);
+
+	engine = Engine.create();
+	world = engine.world;
+  
+  paper = new Paper(200, 200, 65);
+  ground = new Ground(width/2, 470, width, 20);
+
+  dustbin = createSprite(700, 370);
+  dustbin.addImage(dustbin_Image);
+  dustbin.scale = 0.55;
+
+  leftBody = new Dustbin(643, 385, 20, 180);
+  rightBody = new Dustbin(760, 385, 20, 180);
+  bottomBody = new Dustbin(700, 450, 130, 20);
+
+	Engine.run(engine);
+}
+
+function draw() {
+  background(135, 0, 255);
+
+  Engine.update(engine);
+  
+  paper.display();
+  leftBody.display();
+  rightBody.display();
+  bottomBody.display();
+  ground.display();
+
+  drawSprites();
+}
+function keyPressed(){
+  if (keyCode === UP_ARROW){
+    Matter.Body.applyForce(paper.body, paper.body.position, {x : 40, y : -88});
+  }
+}
